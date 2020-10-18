92893d97b6693a102602b27164255bfd032b4977
Ahnaf Tazwar Ayub <tazwarahnaf22@gmail.com>
2020-10-17 20:51:44 -0700

    Detecting coords  on im

--- app/pages/exercises/index.js
+++ app/pages/exercises/index.js
@@ -3,19 +3,25 @@ import React, { Component } from 'react'
 import Webcam from 'react-webcam'
 import Processor from '../../components/processor'
 
+//Backend
+import * as tfjs from "@tensorflow/tfjs";
+import * as tf from "@tensorflow/tfjs-core";
+import * as posenet from "@tensorflow-models/posenet";
+
+
 export class Exercises extends Component {
     constructor(props) {
         super(props);
-
     }
 
     componentDidMount() {
-        // this.getPoseDetails()
+        this.getPoseDetails()
     }
+
 
     async getPoseDetails(){
 
-        const posenet = require('@tensorflow-models/posenet');
+        // const posenet = require('@tensorflow-models/posenet');
     
         async function estimatePoseOnImage(imageElement) {
         // load the posenet model from a checkpoint
@@ -29,34 +35,56 @@ export class Exercises extends Component {
     
         const imageElement = document.getElementById('pose');
         const pose = await estimatePoseOnImage(imageElement);
-        
-    
-        console.log("Pose :", pose);
+        this.plotPoints(pose);
+                
+
+
+    }
+
+
+    plotPoints(pose){
+
+        const canvas = document.getElementById("canvas");
+        const ct = canvas.getContext("2d");
+
+        const points = pose.keypoints;
+        // loop through the keypoints (check posenet for numbering -> location)
+        for (let i = 0; i < points.length; i++) {
+        const key = points[i];
+        if (key.score > 0.0) {
+            console.log(key.position);
+            ct.beginPath();
+            ct.arc(key.position.x, key.position.y, 10, 0, 2 * Math.PI);
+            ct.stroke();
+        }
+      }
     }
 
 
 
 
     render() {
+
+
+
+
         return  <div>
                     <h1>Exercises</h1>
                     <br></br>
                     <p> Get all the excercises avaiable and show em all </p>
                     <br />
 
-                    <img src={'/pose4.png'} id="pose" />
-
-
-                    {/* <Webcam mirrored='true' style={{height: 'auto', width:'auto'}} /> */}
-
+                    <div id="container">
+                        <img src={'/pose4.png'} id="pose" width="300" height="300" />
+                        <canvas id="canvas" width="300" height="300"></canvas>
+                    </div>
+                    
 
                     <h2>
                         <Link href="/">
                             <a>Back to home</a>
                         </Link>
                     </h2>
-
-                    <Processor/>
 
                 </div>         
     }

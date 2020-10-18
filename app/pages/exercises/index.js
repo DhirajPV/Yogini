b7889ae394422080e30133ac25c46f17eb91a407
Ahnaf Tazwar Ayub <tazwarahnaf22@gmail.com>
2020-10-17 21:40:37 -0700

    Fixing pose angles

--- app/pages/exercises/index.js
+++ app/pages/exercises/index.js
@@ -1,22 +1,110 @@
+
+
 import Link from 'next/link'
+import React, { Component } from 'react'
 import Webcam from 'react-webcam'
-import Processor from '../../components/processor'
 import styles from "../../styles/Home.module.css";
 
 
-export default function Exercise() {
-    return  <div className={styles.container}>
-                <h1>Exercises</h1>
-                <br></br>
-                <p> Get all the excercises avaiable and show em all </p>
-                <br/>
-                <h2>
-                    <Link href="/">
-                        <a className={styles.card}>
-                            Back to home
-                        </a>
-                    </Link>
-                </h2>
-                <Processor/>
-            </div>
+//Backend
+import * as tfjs from "@tensorflow/tfjs";
+import * as tf from "@tensorflow/tfjs-core";
+import * as posenet from "@tensorflow-models/posenet";
+
+
+export class Exercises extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        this.getPoseDetails()
+    }
+
+
+    async getPoseDetails(){
+
+        // const posenet = require('@tensorflow-models/posenet');
+    
+        async function estimatePoseOnImage(imageElement) {
+        // load the posenet model from a checkpoint
+        const net = await posenet.load();
+    
+        const pose = await net.estimateSinglePose(imageElement, {
+            flipHorizontal: false
+        });
+        return pose;
+        }
+    
+        const imageElement = document.getElementById('pose');
+        const pose = await estimatePoseOnImage(imageElement);
+        this.plotPoints(pose);
+        this.drawJoint(pose);
+                
+    }
+
+
+    plotPoints(pose){
+
+        const canvas = document.getElementById("canvas");
+        const ct = canvas.getContext("2d");
+
+        const points = pose.keypoints;
+        // loop through the keypoints (check posenet for numbering -> location)
+        for (let i = 0; i < points.length; i++) {
+        const key = points[i];
+        if (key.score > 0) {
+            // console.log(key.position);
+            ct.beginPath();
+            ct.arc(key.position.x, key.position.y, 10, 0, 2 * Math.PI);
+            ct.stroke();
+        }
+      }
+    }
+
+
+
+    drawJoint(pose){
+
+        const points = pose.keypoints;
+
+        for(let i=0; i < points.length ; i++){
+
+            let {part, score, pos} = points[i]
+
+            if (score > 0){
+                console.log("Joint ", points[i]);
+            }
+            
+        }
+
+
+
+    }
+
+
+
+    render() {
+        return  <div>
+                    <h1>Exercises</h1>
+                    <br></br>
+                    <p> Get all the excercises avaiable and show em all </p>
+                    <br />
+
+                    <div id="container">
+                        <img src={'/pose3.png'} id="pose" width="300" height="300" />
+                        <canvas id="canvas" width="300" height="300"></canvas>
+                    </div>
+                    
+
+                    <h2>
+                        <Link href="/">
+                            <a>Back to home</a>
+                        </Link>
+                    </h2>
+
+                </div>         
+    }
 }
+
+export default Exercises;

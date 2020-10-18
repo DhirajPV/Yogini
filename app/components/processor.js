43b55a3dc37bba37b502a356e5748cf7e711d8f4
Dhiraj Pakala Venkata <dhirajpakalavenkata@MacBook-Pro.local>
2020-10-17 18:50:37 -0700

    Posenet in a working component and initial keypoints

--- app/components/processor.js
+++ app/components/processor.js
@@ -1,48 +1,73 @@
-import React, { Component, useEffect } from 'react'
+import React, { Component } from 'react'
 import Webcam from 'react-webcam'
-import PropTypes from 'prop-types'
+import * as tfjs from "@tensorflow/tfjs"
+import * as tf from "@tensorflow/tfjs-core"
+import * as posenet from "@tensorflow-models/posenet"
 
 export class Processor extends Component {
     constructor(props) {
         super(props);
-        this.webCam = React.createRef();
-        this.poseNet;
-        this.ml5; 
-
-        this.state = {
-            detected: false,
-            isModelLoaded: false,
-        };
+        this.WebcamRef = React.createRef(null);
     }
 
-    initializePoseNetModel = () => {
-        this.poseNet = this.ml5.poseNet(this.webCam.current, () => {
-            console.log("Model Initilaized");
-            
-            this.setState({
-                isModelLoaded: true,
-            });
+    drawPoints = (pose, colour) => {
+        const canvas = document.getElementById("canvas");
+        const ct = canvas.getContext("2d");
+        ct.clearRect(0, 0, canvas.width, canvas.height);
+        const points = pose.keypoints;
+        // loop through the keypoints (check posenet for numbering -> location)
+        for (let i = 0; i < points.length; i++) {
+            const key = points[i];
+            if (key.score > 0.1) {
+                console.log(key.position);
+                ct.beginPath();
+                ct.arc(key.position.x, key.position.y, 2, 0, 2 * Math.PI);
+                ct.fillStyle = colour;
+                ct.stroke();
+                ct.fill();
+            }
+        }
+    }
 
-            /*this.poseNet.on('pose', (result) => {
-                this.poses = result;
-                this.setState({
-                    detected: result !== undefined && result.length > 0 ? true : false
-                })
-            });*/
+    drawLines = (pose, colour) => {
 
+    }
+
+    drawCameraIntoCanvas = () => {
+        // Draw the video element into the canvas
+        ctx.drawImage(video, 0, 0, 640, 480);
+        // We can call both functions to draw all keypoints and the skeletons
+        drawKeypoints();
+        drawSkeleton();
+        window.requestAnimationFrame(drawCameraIntoCanvas);
+    }
+
+    runPose = async (net) => {
+        if (this.WebcamRef.current !== undefined && this.WebcamRef.current !== null && 
+          this.WebcamRef.current.video.readyState === 4 ) {
+            const curFrame = this.WebcamRef.current.video;
+            curFrame.width = this.WebcamRef.current.video.videoWidth;
+            curFrame.height = this.WebcamRef.current.video.videoHeight;
+            const pose = await net.estimateSinglePose(curFrame);
+            this.drawPoints(pose);
+        }
+      }
+    
+    runNet = async () => {
+        const net = await posenet.load({
+          architecture: "MobileNetV1",
+          outputStride: 16,
+          inputResolution: { width: 640, height: 480 },
+          multiplier: 0.75,
         });
-
-        console.log('this.poseNet: ', this.poseNet)
+    
+        setInterval(() => {
+          this.runPose(net);
+        }, 150);
     }
 
     componentDidMount() {
-        const script = document.createElement("script");
-        script.src = "https://unpkg.com/ml5@0.5.0/dist/ml5.min.js";
-        script.type = "text/javascript";
-        document.body.appendChild(script);
-
-        this.ml5 = require('ml5')
-        this.initializePoseNetModel();
+        this.runNet();
     }
 
     render() {
@@ -51,9 +76,12 @@ export class Processor extends Component {
             <Webcam 
                 mirrored = 'true' 
                 style = {{height: 'auto', width:'auto'}} 
-                ref = {this.webCam}
+                ref = {this.WebcamRef}
 
             />
+            <canvas id="canvas" width="640" height="480">
+            {" "}
+            </canvas>
             </div>
         )
     }
